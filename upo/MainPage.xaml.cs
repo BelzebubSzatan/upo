e434127d2c4d41b77f8943d42f9cbc505a7d4041
KacperMatlag <matlagkacper@gmail.com>
2024-02-04 19:34:40 +0100

    Add basic propertioes to MainPage

--- upo/MainPage.xaml.cs
+++ upo/MainPage.xaml.cs
@@ -4,12 +4,20 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using upo.models;
 using Xamarin.Forms;
 
 namespace upo {
     public partial class MainPage : ContentPage {
+        Deck deck=new Deck();
+        List<Card> playerCards = new List<Card>();
+        List<Card> computerCards= new List<Card>();
+        bool playerAction = true;
+        bool win=false;
         public MainPage() {
             InitializeComponent();
+            playerCards = deck.GeneratePlayerCards(3);
+            computerCards = deck.GeneratePlayerCards(3);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
